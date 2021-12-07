91a654ae3ee04ed0890b61cf2a9809ee6754f5dd
Alina Tuktarova <alina.tuktarova@corp.codeinside.ru>
2021-12-07 15:51:04 +0300

    Добавление сортировки

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -3,35 +3,95 @@ import { completeTask, deleteTask } from '../../actions/tasks';
 import Button from '../Button/Button';
 import { connect } from 'react-redux';
 import styles from './Task.module.scss';
+import Checkbox from '../Checkbox/Checkbox';
 
 class Task extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { complete: true, done: true };
+  }
   handleDelete = (id) => {
     this.props.deleteTask(id);
   };
   handleComplete = (id) => {
     this.props.completeTask(id);
   };
-
+  handleCheckComplete = () => {
+    this.setState({ complete: !this.state.complete });
+  };
+  handleCheckDone = () => {
+    this.setState({ done: !this.state.done });
+  };
   render() {
     return (
-      <div className={styles.taskContainer}>
-        {this.props.tasks &&
-          this.props.tasks.map((el) => (
-            <div className={styles.line}>
-              <div className={styles.text}>{el.text}</div>
-              <Button
-                classStyle={!el.completed ? 'complete' : 'done'}
-                text={!el.completed ? 'Complete' : 'Done'}
-                onClick={() => this.handleComplete(el.id)}
-              />
-              <Button
-                classStyle="delete"
-                text="X"
-                onClick={() => this.handleDelete(el.id)}
-              />
-            </div>
-          ))}
-      </div>
+      <>
+        <div className={styles.checkboxContainer}>
+          <Checkbox
+            text="Complete"
+            checked={this.state.complete}
+            onChange={() => this.handleCheckComplete()}
+          />
+          <Checkbox
+            text="Done"
+            checked={this.state.done}
+            onChange={() => this.handleCheckDone()}
+          />
+        </div>
+        <div className={styles.taskContainer}>
+          {this.props.tasks &&
+            this.props.tasks.map((el) => {
+              if (this.state.complete && this.state.done) {
+                return (
+                  <div className={styles.line} key={el.id}>
+                    <div className={styles.text}>{el.text}</div>
+                    <Button
+                      classStyle={!el.completed ? 'complete' : 'done'}
+                      text={!el.completed ? 'Complete' : 'Done'}
+                      onClick={() => this.handleComplete(el.id)}
+                    />
+                    <Button
+                      classStyle="delete"
+                      text="X"
+                      onClick={() => this.handleDelete(el.id)}
+                    />
+                  </div>
+                );
+              } else if (this.state.complete && el.completed === false) {
+                return (
+                  <div className={styles.line} key={el.id}>
+                    <div className={styles.text}>{el.text}</div>
+                    <Button
+                      classStyle={!el.completed ? 'complete' : 'done'}
+                      text={!el.completed ? 'Complete' : 'Done'}
+                      onClick={() => this.handleComplete(el.id)}
+                    />
+                    <Button
+                      classStyle="delete"
+                      text="X"
+                      onClick={() => this.handleDelete(el.id)}
+                    />
+                  </div>
+                );
+              } else if (this.state.done && el.completed === true) {
+                return (
+                  <div className={styles.line} key={el.id}>
+                    <div className={styles.text}>{el.text}</div>
+                    <Button
+                      classStyle={!el.completed ? 'complete' : 'done'}
+                      text={!el.completed ? 'Complete' : 'Done'}
+                      onClick={() => this.handleComplete(el.id)}
+                    />
+                    <Button
+                      classStyle="delete"
+                      text="X"
+                      onClick={() => this.handleDelete(el.id)}
+                    />
+                  </div>
+                );
+              } else return <></>;
+            })}
+        </div>
+      </>
     );
   }
 }
